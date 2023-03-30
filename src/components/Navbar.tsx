1493765f1fbcd81818bbdd56bab7a91625c10f8b
umar77805 <ahmedumar661@gmail.com>
2023-03-30 15:44:03 +0530

    added a navbar and implemented dark mode

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import { Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+const Navbar = () => {
+  return <Image src={logo} boxSize="60px" />;
+};
+
+export default Navbar;
