c0da35d79d78b0cbf99efe5ca0ed7b8a7c4aa06d
umar77805 <ahmedumar661@gmail.com>
2023-04-02 13:29:15 +0530

    Created a custom hook to fetch games and abort

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GameResponse {
-  id: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GameResponse>("/games")
-      .then((response) => setGames(response.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { error, games } = useGames();
   return (
     <>
       {error && <p>{error}</p>}
