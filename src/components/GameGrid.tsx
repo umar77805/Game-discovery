57d629fd43738d687ccf50e05594f7d9248ea760
Mohammad Umar Ahmed <mohdumar@lentra.ai>
2025-10-24 18:51:15 +0530

    implemented infinite scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { Fragment } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Query } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
@@ -10,37 +11,51 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { error, data, status, fetchNextPage, isFetchingNextPage } =
-    useGames(gameQuery);
+  const {
+    error,
+    data,
+    status,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   // const skeletonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const skeletonArray = Array.from({ length: 15 }, (_ele, idx) => idx + 1);
+  const fetchedLengthSoFar =
+    data && data.pages
+      ? data.pages.reduce((acc, curr) => curr.results.length + acc, 0)
+      : 0;
+
   return (
-    <Box padding={2}>
+    <>
       {error && <p>{error.message}</p>}
-      <SimpleGrid spacing={3} columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}>
-        <>
-          {status === "pending" &&
-            skeletonArray.map((skeleton) => <GameSkeleton key={skeleton} />)}
-          {data && data.pages && data.pages.length
-            ? data.pages.map((page, idx) => (
-                <Fragment key={idx}>
-                  {page.results.map((game) => (
-                    <GameCard key={game.id} game={game} />
-                  ))}
-                </Fragment>
-              ))
-            : null}
-        </>
-      </SimpleGrid>
-      <Button
-        onClick={() => fetchNextPage()}
-        variant="outline"
-        isLoading={isFetchingNextPage}
-        marginY={5}
+      <InfiniteScroll
+        dataLength={fetchedLengthSoFar}
+        next={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<Spinner marginY={5} />}
       >
-        Load More
-      </Button>
-    </Box>
+        <SimpleGrid
+          padding={2}
+          spacing={3}
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        >
+          <>
+            {status === "pending" &&
+              skeletonArray.map((skeleton) => <GameSkeleton key={skeleton} />)}
+            {data && data.pages && data.pages.length
+              ? data.pages.map((page, idx) => (
+                  <Fragment key={idx}>
+                    {page.results.map((game) => (
+                      <GameCard key={game.id} game={game} />
+                    ))}
+                  </Fragment>
+                ))
+              : null}
+          </>
+        </SimpleGrid>
+      </InfiniteScroll>
+    </>
   );
 };
 
